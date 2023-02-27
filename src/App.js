eb669d0a6fc85707e08f1b32dadf156a19d7f55a
trueotmor <proskurin.sasha78@gmail.com>
2023-02-27 16:33:23 +0300

    added toggle &telegram hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const {tg, onToggleButton} = useTelegram()
+
   useEffect(()=>{
     tg.ready();
-  })
+  },[])
 
   return (
     <div className="App">
-      Работает
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
