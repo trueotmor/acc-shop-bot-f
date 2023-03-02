c3dc91197c381d983fee4ef5805b5914a984d360
trueotmor <proskurin.sasha78@gmail.com>
2023-03-02 16:38:12 +0300

    form ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
   const {tg, onToggleButton} = useTelegram()
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Form.css'
 
 
-const ProductList = () => {
+const Form = () => {
+
+    const [country, setCountry] = useState('')
+    const [street, setStreet] = useState('')
+    const [subject, setSubject] = useState('physical')
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
 
     return (
-        <div className=''>
-            FORM
+        <div className='form'>
+            <h3>Введите ваши данные</h3>
+            <input 
+                className={'input'} 
+                type='text' 
+                placeholder={'Страна'}
+                value= {country}
+                onChange={onChangeCountry}
+            />
+            <input 
+                className={'input'}
+                type='text' 
+                placeholder={'Улица'} 
+                value= {street}
+                onChange={onChangeStreet}/>
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Юр. лицо</option>
+                <option value={'legal'}>Физ. лицо</option>
+            </select>
         </div>
     )
 }
 
-export default ProductList;
+export default Form;
