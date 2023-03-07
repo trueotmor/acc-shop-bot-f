7545fb117031ed1a9a18cfd48dfcae9c5e488ad6
trueotmor <proskurin.sasha78@gmail.com>
2023-03-07 15:56:52 +0300

    dddd

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -12,23 +12,6 @@ const ProductList = () => {
             Product List
             <div className='form'>
                 <h3>Введите ваши данные</h3>
-                <input 
-                    className={'input'} 
-                    type='text' 
-                    placeholder={'Страна'}
-                    value= {country}
-                    onChange={onChangeCountry}
-                />
-                <input 
-                    className={'input'}
-                    type='text' 
-                    placeholder={'Улица'} 
-                    value= {street}
-                    onChange={onChangeStreet}/>
-                <select value={subject} onChange={onChangeSubject} className={'select'}>
-                    <option value={'physical'}>Юр. лицо</option>
-                    <option value={'legal'}>Физ. лицо</option>
-                </select>
             </div>
         </div>
     )
