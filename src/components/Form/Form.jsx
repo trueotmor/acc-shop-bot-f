b4183f14b1d08b6d812ae8acf62f7fb3d42d07b1
trueotmor <proskurin.sasha78@gmail.com>
2023-03-10 16:56:14 +0300

    add

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,21 +10,21 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
-    // const onSendData = useCallback(()=>{
-    //     const data = {
-    //         country,
-    //         street,
-    //         subject,
-    //     };
-    //     tg.sendData(JSON.stringify(data));
-    // }, [country, street, subject]);
+    const onSendData = useCallback(()=>{
+        const data = {
+            country,
+            street,
+            subject,
+        };
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject]);
 
-    // useEffect(()=>{
-    //     tg.onEvent('mainButtonClicked', tg.sendData('asdasda'));
-    //     return () => {
-    //         tg.offEvent('mainButtonClicked', onSendData);
-    //     }
-    // }, [onSendData]);
+    useEffect(()=>{
+        tg.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        }
+    }, [onSendData]);
 
     useEffect(()=>{
         tg.MainButton.setParams({
