1132766cd518a1e6386bbe6b9fe9d337b18bc795
trueotmor <proskurin.sasha78@gmail.com>
2023-03-21 16:50:14 +0300

    adad

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId, 
         };
-        fetch('https://acc-shop.herokuapp.com:8000', {
+        fetch('https://acc-shop.herokuapp.com:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/JSON',
